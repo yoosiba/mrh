f5bf72a97e09c7d2518c039cb5515fd9bead3a75
Jakub Siberski <944287+yoosiba@users.noreply.github.com>
2020-05-12 18:53:54 +0200

    move term variable

--- release.sh
+++ release.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+TERM=xterm
+
 build_dist() {
   rm -rf bin
   mkdir bin
@@ -7,7 +9,6 @@ build_dist() {
 }
 
 test_dist() {
-  local TERM=xterm
   unzip ./bin/mrh.zip -d./bin/mrh
   ./bin/mrh/mrh.bash || exit 1
 }
