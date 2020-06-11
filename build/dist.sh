bce5ffda66bce2471ecd2353108d3e9491293b80
Jakub Siberski <944287+yoosiba@users.noreply.github.com>
2020-06-12 01:10:35 +0200

    cleanup

--- build/dist.sh
+++ build/dist.sh
@@ -14,19 +14,14 @@ build_dist() {
 }
 
 test_dist() {
-  pwd && ls -la
   mkdir ./bin/mrh_dist_test
   cp ./bin/mrh.zip ./bin/mrh_dist_test/
-  # pushd ./bin/mrh_dist_test >/dev/null || exit 62
-  pwd && ls -la
   unzip ./bin/mrh_dist_test/mrh.zip -d ./bin/mrh_dist_test/mrh
-  pwd && ls -la
+
   ./bin/mrh_dist_test/mrh/mrh.bash
   ./bin/mrh_dist_test/mrh/mrh.bash -ud
-  # popd >/dev/null || exit 61
-  pwd && ls -la
+
   rm -rf ./bin/mrh_dist_test
-  pwd && ls -la
 }
 
 dist() {
